6c2645a1fd49e0393668eb466564436074033887
Kdu <83893756+Kadu-H@users.noreply.github.com>
2022-03-21 17:28:49 -0300

    v4.0

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const ponteiroMinutos = document.querySelector(".ponteiro.minutos")
 const ponteiroHoras = document.querySelector(".ponteiro.horas")
 
 
+const setRotation = (element, rotationPercentage) => {
+    element.style.setProperty("--rotation", rotationPercentage * 360)
+}
+
+
 const setClock = () =>{
     const atualDate = new Date()
 
     const secondsPercentage = atualDate.getSeconds() / 60
     const minutesPercentage = atualDate.getMinutes() / 60
-    const hoursPercentage = atualDate.getHours() / 60
+    const hoursPercentage = atualDate.getHours() / 12
+
+    setRotation(ponteiroSegundo, secondsPercentage)
+    setRotation(ponteiroMinutos, minutesPercentage)
+    setRotation(ponteiroHoras, hoursPercentage)
 
 }
+setClock()
+
+setInterval(setClock,1000)
